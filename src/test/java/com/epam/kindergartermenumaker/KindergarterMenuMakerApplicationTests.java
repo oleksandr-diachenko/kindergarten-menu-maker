4491d657a743936e6666f279b2e41ae656097abb
oleksandr-diachenko <mail.positiv@gmail.com>
2020-09-17 08:52:16 +0300

    [KMM-1] Setup Spring boot project and create repository

---
+++ src/test/java/com/epam/kindergartermenumaker/KindergarterMenuMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.kindergartermenumaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindergarterMenuMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

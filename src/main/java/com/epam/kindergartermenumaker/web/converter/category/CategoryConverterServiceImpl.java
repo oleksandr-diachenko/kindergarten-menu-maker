788a114d8c1fd02a3d73d027e778cb1881a45177
oleksandr-diachenko <mail.positiv@gmail.com>
2020-09-30 16:44:34 +0300

    [KMM-29] Fixed displaying empty category without recipes

--- src/main/java/com/epam/kindergartermenumaker/web/converter/category/CategoryConverterServiceImpl.java
+++ src/main/java/com/epam/kindergartermenumaker/web/converter/category/CategoryConverterServiceImpl.java
@@ -4,13 +4,12 @@ import com.epam.kindergartermenumaker.bussiness.service.logging.CategoryService;
 import com.epam.kindergartermenumaker.dao.entity.Category;
 import com.epam.kindergartermenumaker.web.converter.Converter;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.apache.commons.collections4.CollectionUtils.*;
+import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 
 /**
  * @author : Oleksandr Diachenko
